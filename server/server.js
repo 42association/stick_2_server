919eb51a90853e5ce97412939842a0aead5fca35
kkaneta <knt.khe@proton.me>
2024-04-06 16:34:28 +0900

    サーバーのパッケージとポート番号を更新

--- server/server.js
+++ server/server.js
@@ -5,15 +5,13 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/api/data', (req, res) => {
-	const message = req.body.message;
-	console.log('Received message:', message);
-
-	// 処理
-
-	res.send('Message received');
+  const message = req.body.message;
+  console.log('Received message:', message);
+  // 処理
+  res.send('Message received');
 });
 
-const port = 8080;
+const port = 4242;
 app.listen(port, () => {
-	console.log(`Server started on port ${port}`);
+  console.log(`Server started on port ${port}`);
 });
